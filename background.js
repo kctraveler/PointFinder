32ac7d0638f066d9703c13f6d895300b585fce88
Shane Panchot <shane.panchot@gmail.com>
2022-04-30 20:12:36 -0500

    add async to getRequest; reply to msg w/ promise

--- background.js
+++ background.js
@@ -5,17 +5,41 @@
 //const baseUri = 'http://localhost:3000/';
 const baseUri = 'https://point-finder.herokuapp.com/'
 
-const getRequest = (urlPath, callback) => {
-    console.log(`Making request to ${urlPath}`);
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        callback(this);
-    }
-    xhr.open('GET', urlPath, true);
-    xhr.send(null);
+/**
+ * Makes a get request and returns a promise.
+ * @param {String} urlPath - URL to make get request to
+ */
+const getRequest = (urlPath) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', urlPath, true);
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve({
+                    status: xhr.status,
+                    responseText: xhr.response
+                });
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+            }
+        }
+        xhr.onerror = () => {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
 }
 
-//Update the browserAction button to indicate a deal was found
+/**
+ * Sets an alert on the BrowserAction button for the given id.
+ * @param {int} tabId - the id of the tab to set alert on
+ */
 const alertDeal = (tabId) => {
     browser.browserAction.setBadgeText({
         text: "!",
@@ -31,7 +55,10 @@ const alertDeal = (tabId) => {
     });
 }
 
-// Reset the browserAction button to show a deal was not found. 
+/**
+ * Removes alert on BrowserAction for given tabid.
+ * @param {int} tabId - id of the tab to remove alert
+ */
 const resetAlert = (tabId) => {
     browser.browserAction.setBadgeText({
         text: "",
@@ -47,40 +74,55 @@ const resetAlert = (tabId) => {
     });
 }
 
-
 /**
- * Updates the extension components according to the deal
+ * Updates browser alerts and responds with promo details
+ * @param {int} tabId - id of tab being updated
+ * @param {Promise} response Promise from a getRequest
  */
-const updateBrowser = (tabId, domain) => {
-    if (!domain) {
-        resetAlert(tabId);
-        return false;
+const updateBrowser = async (tabId, response) => {
+    let res = await response;
+    console.log(res);
+    if (res.status === 200) {
+        alertDeal(tabId);
+        console.log(200)
+    } else {
+        resetAlert(tabId)
     }
-    getRequest(`${baseUri}merchant/${domain}`, (res) => {
-        let status = res.status;
-        if (status === 200) {
-            alertDeal(tabId);
-            return JSON.parse(res.responseText)
-        } else {
-            resetAlert(tabId)
-            return false;
-        }
-    });
 }
 
+/**
+ * Gets the available promotions from the backend api for the 
+ * tabs url. 
+ * @param {String} tabUrl - the url of the users tab.
+ * @return {Promise} Promise from the get request.
+ */
+let getPromos = async (tabUrl) => {
+    const regEx = new RegExp(/(?!www)\b(\w+\.{0,1})[0-9a-zA-Z\-]*(?=(\.com[$/.\*]|\.org[$/.\*]|\.net[$/.\*]))/);
+    let tabDomain = regEx.exec(tabUrl);
+    if (tabDomain) {
+        tabDomain = tabDomain[0];
+        let url = `${baseUri}merchant/${tabDomain}`;
+        return getRequest(url)
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Handles messages from the content script requesting available promotions
+ * for the tabs URL.
+ * Updates the Browser Action alert
+ * Returns a promise to the content script resolving to the status and text from
+ * the request.
+ */
 browser.runtime.onMessage.addListener((message, sender) => {
     if (message.type === 'get_promos') {
-        let senderId = sender.tab.id;
-        let url = sender.tab.url;
+        const senderId = sender.tab.id;
+        const url = sender.tab.url;
         console.log(`MESSAGE RECEIVED\n` +
             `Type: ${message.type}\nFrom Id: ${senderId}\nURL: ${url}`);
-        const regEx = new RegExp('[0-9a-zA-Z\-]*(?=(\.com\/|\.org\/))');
-        let domain = regEx.exec(url);
-        if (domain) {
-            domain = domain[0];
-        } else {
-            domain = null;
-        }
-        return updateBrowser(senderId, domain);
+        let resPromise = getPromos(url);
+        updateBrowser(senderId, resPromise);
+        return resPromise;
     }
 })
